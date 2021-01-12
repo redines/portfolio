6d5f819194c14e8f4d1ff8ad841e3ba5c2c2ac4b
pontus <pontus.g91@gmail.com>
2021-01-12 17:57:04 +0100

    cleaned up and added a test header component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header></Header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin:0px;
+  padding:0px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Pontus Portfolio</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+h1 {
+    text-align: center;
+}
+
+</style>
